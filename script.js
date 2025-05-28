0a9ed246f1d78c1c51982079a2da57679eca67d6
ThenamXe0n <thenameet0@gmail.com>
2025-05-28 21:28:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+//variables and keywords
+
+// following are some reserved keywords in javascript
+// console
+// document
+// let
+// var
+// const
+// function
+// Number
+// class
+
+// ==================================================================
+
+// - variable are user define keyword
+// - variable are used to store data
+// vaiable have a name and a value linked to its name i.e (referrence)
+
+// ======================================================================
+
+//how to create variable in js
+// studentName = "anjali" //string / X do not use or declare variable with this method
+
+/* note : declaring variable without keyword will not give any error but it is not a good way or not recommended by javascript to declare variables without keyword(let,var,const)
+to declare a vari we can use keyword let,var,const
+*/
+
+//let define first variable with correct word
+// var studentName;
+let a;
+console.log(a);
+a = "mango";
+console.log(a);
+
+const b = "banana";
+console.log(b);
+b = "apple";
+console.log(b);
+
+let console = "hi";
+
+//var - variable redeclare and reinitilize (reassign)
+//let - cannot redeclare but can be reassign or()
+//const - cannot redeclare and cannot reassign
+
+/*
+// note : 
+// rules to name a variable
+           * do not start name of variable with symbols like # ,@,_,* ,&,$ etc
+           * do not start name of variable with number
+           * do not use space in variable name
+           * do not use special character in variable name
+           * do not use keyword in variable name
+           * do not use same name for two variable
+           * do not use same name for variable and function
+*/
