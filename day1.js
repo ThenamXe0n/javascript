33f002eae0348f2591c92b61b3e1352d28308b26
ThenamXe0n <thenameet0@gmail.com>
2025-05-29 19:46:37 +0530

    day 1 js variables , hoisting and TDZ

---
+++ day1.js
@@ -0,0 +1,40 @@
+// person1 = "app";
+// var person1 = "tohid";
+// // console.log(person+" "+person1+"        "+"test"+" "+2+2)
+// // variable declared by let are block scope // can be redeclared in different scope
+// //var = global
+
+// {
+//   var person1 = "ram";
+//   console.log(person1);
+// }
+// console.log(person1);
+
+// const statement = "sun rises from east";
+
+// {
+//   const statement = "javascript is a funny programming language";
+//   console.log("comming form block:", statement);
+// }
+// console.log("comming from global area global execution context", statement);
+
+// //hoisting... and TDZ(temporal dead zone)
+
+// // -JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables, classes, or imports to the top of their scope, prior to execution of the code.
+
+console.log(a)
+var a=5;
+console.log(a) console.log("hellow")
+
+// TDZ(temporal dead zone)
+
+
+
+
+
+// // 1 console the following statement 
+// -  “We cannot solve problems with the kind of thinking we employed when we came up with them.” —Albert Einstein
+
+
+
+
