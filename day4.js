8943620202194ca513250e8fb74de48228d1ac60
ThenamXe0n <thenameet0@gmail.com>
2025-06-03 19:55:21 +0530

    `Refactored day4.js with updated function definitions and calls`

--- day4.js
+++ day4.js
@@ -18,43 +18,68 @@
 // console.log(greeting);
 
 //create a greeting function which will take username and greet the user
-function greetuser(user="user",greet) {
- 
+function greetuser(user = "user", greet) {
   let greeting = `hi! ${greet} ${user} `;
   console.log(greeting);
 }
 // greetuser("dheeraj")
 // greetuser("sajid")
 // greetuser("anjali")
-greetuser()
-greetuser("sajid","good afternoon")
-greetuser("dheeraj","good evening")
-greetuser("good morning","ankita")
+greetuser();
+greetuser("sajid", "good afternoon");
+greetuser("dheeraj", "good evening");
+greetuser("good morning", "ankita");
 
-
-function add(num1,num2){
-let sum = num1+num2
-return sum
-console.log("hi after")
-console.log(`addition of num1 and num2 is ${sum}`)
+function add(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+  console.log("hi after");
+  console.log(`addition of num1 and num2 is ${sum}`);
 }
 // console.log(sum)
 
-let result =add(5,5)
-console.log(result)
+// let result =add(5,5)
+// console.log(result)
 
-//create a function which will calculate area of squre
-function areaOfSqure(s){
-let area = s*s
-return `sqare of given s ${s} is ${area} sq.cm`
+// //create a function which will calculate area of squre
+// function areaOfSqure(s){
+// let area = s*s
+// return `sqare of given s ${s} is ${area} sq.cm`
+// }
+// console.log(areaOfSqure(10))
+// console.log(areaOfSqure(5))
+
+// function areaOfCircle(){
+//     let r = prompt("enter radius in number")
+//     let formula = 22/7*r**2
+//     return `<h1>area of circle with radius ${r} is ${formula}</h1>`
+// }
+
+// document.writeln(areaOfCircle())
+
+// Types of funtions in javascript
+// 1 regular function
+// 2 function expression(category) = are those function which are stored in a variable
+// 3 Anonymous Functions
+// 4 arrow functions
+//5 IIFE(imidiatly invoke function expression)
+(function () {
+  console.log("iife");
+})();
+
+// console.log(a("khushi"));
+let veg3 = "gobhi"
+function dinner(veg1, veg2) {
+  veg3 = "abcd"
+  return `cooked ${veg1} ${veg2} ${veg3} for dinner`;
 }
-console.log(areaOfSqure(10))
-console.log(areaOfSqure(5))
+console.log(dinner("aalu","methi")) //impure function
+console.log(veg3)
 
-function areaOfCircle(){
-    let r = prompt("enter radius in number")
-    let formula = 22/7*r**2
-    return `<h1>area of circle with radius ${r} is ${formula}</h1>`
+
+//callback function //
+function b(){
+
 }
 
-document.writeln(areaOfCircle())
+
---
+++ day5.js
@@ -0,0 +1,8 @@
+// string and its methods
+let str1="mindcoder"
+let str = "javascript has java predefined function also refer as methods"
+//how to find no of letter in a sring 
+console.log(str1.length)
+console.log(str.replaceAll("j","html"))
+console.log(str.includes("jaVascript"))
+console.log(str.toUpperCase())
